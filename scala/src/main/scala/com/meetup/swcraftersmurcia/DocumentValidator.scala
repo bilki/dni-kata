06cf57c922df89ae1bfa465b1578166d1a42eb5d
Roberto Serrano <r.semarcos@gmail.com>
2022-07-01 18:59:19 +0200

    Validates that input prefix is 8 digits and last letter is valid

--- scala/src/main/scala/com/meetup/swcraftersmurcia/DocumentValidator.scala
+++ scala/src/main/scala/com/meetup/swcraftersmurcia/DocumentValidator.scala
@@ -2,12 +2,34 @@ package com.meetup.swcraftersmurcia
 
 object DocumentValidator {
 
-  case class Dni(value: String)
+  object Messages {
+
+    val sizeNotValidMsg = "size not equal to 9"
+    val prefixNotValidMsg = "dni prefix should be all numbers"
+    val prefixEndsWithLetter = "dni should end with a letter"
+
+  }
+
+  sealed abstract class IdentityDoc(value: String)
+
+  case class Dni(value: String) extends IdentityDoc(value)
+
+  case class Nie(value: String) extends IdentityDoc(value)
 
   case class NotValidDni(msg: String)
 
   def validate(rawInput: String): Either[NotValidDni, Dni] =
-    if (rawInput.length != 9) Left(NotValidDni("size not equal to 9"))
-    else Right(Dni(rawInput))
+    if (rawInput.length != 9) Left(NotValidDni(Messages.sizeNotValidMsg))
+    else {
+      val prefix = rawInput.take(8)
+      if (prefix.forall(_.isDigit)) {
+        val lastLetter = rawInput.reverse.head
+        if (Remainder.letter.values.toList.contains(lastLetter))
+          Right(Dni(rawInput))
+        else
+          Left(NotValidDni(Messages.prefixEndsWithLetter))
+      } else
+        Left(NotValidDni(Messages.prefixNotValidMsg))
+    }
 
 }
--- scala/src/test/scala/com/meetup/swcraftersmurcia/KataSpec.scala
+++ scala/src/test/scala/com/meetup/swcraftersmurcia/KataSpec.scala
@@ -1,6 +1,6 @@
 package com.meetup.swcraftersmurcia
 
-import com.meetup.swcraftersmurcia.DocumentValidator.{Dni, NotValidDni}
+import com.meetup.swcraftersmurcia.DocumentValidator.{Dni, Messages, NotValidDni}
 import munit.FunSuite
 
 class KataSpec extends FunSuite {
@@ -8,7 +8,7 @@ class KataSpec extends FunSuite {
   test("DNI or NIE size should return an error if input size is not 9") {
     val input = "123456789X"
 
-    val expected = Left(NotValidDni("size not equal to 9"))
+    val expected = Left(NotValidDni(Messages.sizeNotValidMsg))
 
     val result = DocumentValidator.validate(input)
 
@@ -24,5 +24,27 @@ class KataSpec extends FunSuite {
 
     assertEquals(result, expected)
   }
+
+  test("DNI should return an error if input does not start with 8 digits") {
+    val input = "A2345678X"
+
+    val expected = Left(NotValidDni(Messages.prefixNotValidMsg))
+
+    val result = DocumentValidator.validate(input)
+
+    assertEquals(result, expected)
+  }
+
+  test("DNI should end with a letter") {
+    val input = "123456789"
+
+    val expected = Left(NotValidDni(Messages.prefixEndsWithLetter))
+
+    val result = DocumentValidator.validate(input)
+
+    assertEquals(result, expected)
+  }
+
+
 
 }
