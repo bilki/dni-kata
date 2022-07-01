1df33d1be42c87966b6e41a836a1401b7de5f3b1
Roberto Serrano <r.semarcos@gmail.com>
2022-07-01 18:33:29 +0200

    Validates that input with size 9 returns a DNI

--- scala/src/test/scala/com/meetup/swcraftersmurcia/KataSpec.scala
+++ scala/src/test/scala/com/meetup/swcraftersmurcia/KataSpec.scala
@@ -1,16 +1,28 @@
 package com.meetup.swcraftersmurcia
 
-import com.meetup.swcraftersmurcia.DocumentValidator.NotValidDni
+import com.meetup.swcraftersmurcia.DocumentValidator.{Dni, NotValidDni}
 import munit.FunSuite
 
 class KataSpec extends FunSuite {
 
-  test("DNI or NIE size should be of size 9") {
+  test("DNI or NIE size should return an error if input size is not 9") {
     val input = "123456789X"
 
     val expected = Left(NotValidDni("size not equal to 9"))
 
-    assertEquals(DocumentValidator.validate(input), expected)
+    val result = DocumentValidator.validate(input)
+
+    assertEquals(result, expected)
+  }
+
+  test("DNI or NIE should be of size 9") {
+    val input = "12345678X"
+
+    val expected = Right(Dni(input))
+
+    val result = DocumentValidator.validate(input)
+
+    assertEquals(result, expected)
   }
 
 }
