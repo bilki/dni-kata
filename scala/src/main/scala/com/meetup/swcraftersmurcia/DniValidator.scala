4b31f57b01f5d9d2063ccb3d8394d15c8316f9ed
Roberto Serrano <r.semarcos@gmail.com>
2022-07-02 12:40:39 +0200

    Green - Property: all input must be nine characters long

--- scala/src/main/scala/com/meetup/swcraftersmurcia/DniValidator.scala
+++ scala/src/main/scala/com/meetup/swcraftersmurcia/DniValidator.scala
@@ -1,13 +1,17 @@
 package com.meetup.swcraftersmurcia
 
-import cats.data.ValidatedNec
+import cats.data.{Validated, ValidatedNec}
 import cats.syntax.all._
+import com.meetup.swcraftersmurcia.DniError.NotNineLong
 
 object DniValidator {
 
   case class Dni(value: String)
 
+  private def validateNineCharsLong(input: String): ValidatedNec[DniError, String] =
+    Validated.cond(input.length == 9, input, NotNineLong).toValidatedNec
+
   def validateDNI(rawInput: String): ValidatedNec[DniError, Dni] =
-    Dni(rawInput).validNec[DniError]
+    validateNineCharsLong(rawInput).map(Dni)
 
 }
