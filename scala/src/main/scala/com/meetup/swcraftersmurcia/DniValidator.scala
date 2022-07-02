bd5209b4b966fd1568c70891e7ab9ce2a3ed9d16
Roberto Serrano <r.semarcos@gmail.com>
2022-07-02 13:36:08 +0200

    Green - Property: validation fails for input ending with num

--- scala/src/main/scala/com/meetup/swcraftersmurcia/DniValidator.scala
+++ scala/src/main/scala/com/meetup/swcraftersmurcia/DniValidator.scala
@@ -2,16 +2,21 @@ package com.meetup.swcraftersmurcia
 
 import cats.data.{Validated, ValidatedNec}
 import cats.syntax.all._
-import com.meetup.swcraftersmurcia.DniError.NotNineLong
+import com.meetup.swcraftersmurcia.DniError.{NotLastLetter, NotNineLong}
 
 object DniValidator {
 
   case class Dni(value: String)
 
-  private def validateNineCharsLong(input: String): ValidatedNec[DniError, String] =
-    Validated.cond(input.length == 9, input, NotNineLong).toValidatedNec
+  private def validateNineCharsLong(input: String): ValidatedNec[NotNineLong, String] =
+    Validated.cond(input.length == Constants.DNI_LENGTH, input, NotNineLong).toValidatedNec
+
+  private def validateLastCharNotNum(input: String): ValidatedNec[NotLastLetter, String] =
+    Validated.cond(!input.last.isDigit, input, NotLastLetter).toValidatedNec
 
   def validateDNI(rawInput: String): ValidatedNec[DniError, Dni] =
-    validateNineCharsLong(rawInput).map(Dni)
+    validateNineCharsLong(rawInput)
+      .andThen(validateLastCharNotNum)
+      .map(Dni)
 
 }
