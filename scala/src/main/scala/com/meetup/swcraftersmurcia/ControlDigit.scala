590be698a1221db3e52bfa8050626917b5783f42
Roberto Serrano <r.semarcos@gmail.com>
2022-07-02 12:21:33 +0200

    Add fixtures and replacement dictionaries

---
+++ scala/src/main/scala/com/meetup/swcraftersmurcia/ControlDigit.scala
@@ -0,0 +1,31 @@
+package com.meetup.swcraftersmurcia
+
+object ControlDigit {
+
+  val remainder: Map[Long, Char] = Map(
+    0L  -> 'T',
+    1L  -> 'R',
+    2L  -> 'W',
+    3L  -> 'A',
+    4L  -> 'G',
+    5L  -> 'M',
+    6L  -> 'Y',
+    7L  -> 'F',
+    8L  -> 'P',
+    9L  -> 'D',
+    10L -> 'X',
+    11L -> 'B',
+    12L -> 'N',
+    13L -> 'J',
+    14L -> 'Z',
+    15L -> 'S',
+    16L -> 'Q',
+    17L -> 'V',
+    18L -> 'H',
+    19L -> 'L',
+    20L -> 'C',
+    21L -> 'K',
+    22L -> 'E'
+  )
+
+}
---
+++ scala/src/main/scala/com/meetup/swcraftersmurcia/NieSpecific.scala
@@ -0,0 +1,11 @@
+package com.meetup.swcraftersmurcia
+
+object NieSpecific {
+
+  val replaceDigit: Map[Char, Char] = Map(
+    'X' -> '0',
+    'Y' -> '1',
+    'Z' -> '2'
+  )
+
+}
