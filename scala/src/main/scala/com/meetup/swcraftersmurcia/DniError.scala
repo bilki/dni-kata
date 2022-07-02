64bdc4381551b50d4a498cfdfc5489ea0b999c33
Roberto Serrano <r.semarcos@gmail.com>
2022-07-02 13:12:54 +0200

    Red - Fix prefix generator to use fixed size instead of filtering

--- scala/src/main/scala/com/meetup/swcraftersmurcia/DniError.scala
+++ scala/src/main/scala/com/meetup/swcraftersmurcia/DniError.scala
@@ -5,6 +5,9 @@ sealed abstract class DniError
 object DniError {
 
   case object NotNineLong extends DniError
+  type NotNineLong = NotNineLong.type
+
   case object NotLastLetter extends DniError
+  type NotLastLetter = NotLastLetter.type
 
 }
--- scala/src/test/scala/com/meetup/swcraftersmurcia/Generators.scala
+++ scala/src/test/scala/com/meetup/swcraftersmurcia/Generators.scala
@@ -6,7 +6,7 @@ object Generators {
 
   val notNineCharsGen: Gen[String] = Gen.alphaNumStr.filterNot(_.length == Constants.DNI_LENGTH)
 
-  val validPrefixGen: Gen[String] = Gen.numStr.filter(_.length == Constants.DNI_PREFIX_LENGTH)
+  val validPrefixGen: Gen[String] = Gen.listOfN(Constants.DNI_PREFIX_LENGTH, Gen.numChar).map(_.mkString)
 
   val notLastCharWithNumGen: Gen[String] =
     for {
