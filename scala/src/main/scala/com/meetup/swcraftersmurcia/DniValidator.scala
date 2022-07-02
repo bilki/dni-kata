65dc78075d166ee967abf88945f91a2364486d6f
Roberto Serrano <r.semarcos@gmail.com>
2022-07-02 13:41:51 +0200

    Green - Property: validation fails for input ending with forbidden letter

--- scala/src/main/scala/com/meetup/swcraftersmurcia/DniValidator.scala
+++ scala/src/main/scala/com/meetup/swcraftersmurcia/DniValidator.scala
@@ -2,7 +2,7 @@ package com.meetup.swcraftersmurcia
 
 import cats.data.{Validated, ValidatedNec}
 import cats.syntax.all._
-import com.meetup.swcraftersmurcia.DniError.{NotLastLetter, NotNineLong}
+import com.meetup.swcraftersmurcia.DniError.{NotLastLetter, NotLastValidLetter, NotNineLong}
 
 object DniValidator {
 
@@ -14,9 +14,13 @@ object DniValidator {
   private def validateLastCharNotNum(input: String): ValidatedNec[NotLastLetter, String] =
     Validated.cond(!input.last.isDigit, input, NotLastLetter).toValidatedNec
 
+  private def validateLastCharValidLetter(input: String): ValidatedNec[NotLastValidLetter, String] =
+    Validated.cond(!Constants.INVALID_LAST_LETTERS.contains(input.last), input, NotLastValidLetter).toValidatedNec
+
   def validateDNI(rawInput: String): ValidatedNec[DniError, Dni] =
     validateNineCharsLong(rawInput)
       .andThen(validateLastCharNotNum)
+      .andThen(validateLastCharValidLetter)
       .map(Dni)
 
 }
